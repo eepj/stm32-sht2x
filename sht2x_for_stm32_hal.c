68b4be6b1ddd930919f3e6dffc29dd546fa18c9d
Anonymous <62833867+eepj@users.noreply.github.com>
2020-04-26 17:43:50 +0800

    Add files via upload

---
+++ sht2x_for_stm32_hal.c
@@ -0,0 +1,130 @@
+#include "sht2x_for_stm32_hal.h"
+#include "main.h"
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/**
+ * @brief Initializes the SHT2x temperature/humidity sensor.
+ * @param hi2c User I2C handle pointer.
+ */
+void SHT2x_Init(I2C_HandleTypeDef *hi2c) {
+	_sht2x_ui2c = hi2c;
+}
+
+/**
+ * @brief Gets the value stored in user register.
+ * @return 8-bit value stored in user register, 0 to 255.
+ */
+uint8_t SHT2x_ReadUserReg(void) {
+	uint8_t val;
+	uint8_t cmd = SHT2x_READ_REG;
+	HAL_I2C_Master_Transmit(_sht2x_ui2c, SHT2x_I2C_ADDR << 1, &cmd, 1,
+			SHT2x_TIMEOUT);
+	HAL_I2C_Master_Receive(_sht2x_ui2c, SHT2x_I2C_ADDR << 1, &val, 1,
+			SHT2x_TIMEOUT);
+	return val;
+}
+
+/**
+ * @brief Sends the designated command to sensor and read a 16-bit raw value.
+ * @param cmd Command to send to sensor.
+ * @return 16-bit raw value, 0 to 65535.
+ */
+uint16_t SHT2x_GetRaw(uint8_t cmd) {
+	uint8_t val[3] = { 0 };
+	HAL_I2C_Master_Transmit(_sht2x_ui2c, SHT2x_I2C_ADDR << 1, &cmd, 1,
+			SHT2x_TIMEOUT);
+	while (HAL_I2C_Master_Receive(_sht2x_ui2c, SHT2x_I2C_ADDR << 1, val, 3,
+			SHT2x_TIMEOUT) != HAL_OK)
+		;
+	return val[0] << 8 | val[1];
+}
+
+/**
+ * @brief Measures and gets the current temperature.
+ * @param hold Holding mode, 0 for no hold master, 1 for hold master.
+ * @return Floating point temperature value.
+ */
+float SHT2x_GetTemperature(uint8_t hold) {
+	uint8_t cmd = (hold ? SHT2x_READ_TEMP_HOLD : SHT2x_READ_TEMP_NOHOLD);
+	return -46.85 + 175.72 * (SHT2x_GetRaw(cmd) / 65536.0);
+}
+
+/**
+ * @brief Measures and gets the current relative humidity.
+ * @param hold Holding mode, 0 for no hold master, 1 for hold master.
+ * @return Floating point relative humidity value.
+ */
+float SHT2x_GetRelativeHumidity(uint8_t hold) {
+	uint8_t cmd = (hold ? SHT2x_READ_RH_HOLD : SHT2x_READ_RH_NOHOLD);
+	return -6 + 125.00 * (SHT2x_GetRaw(cmd) / 65536.0);
+}
+
+/**
+ * @brief Sets the measurement resolution.
+ * @param res Enum resolution.
+ */
+void SHT2x_SetResolution(Resolution res) {
+	uint8_t val = SHT2x_ReadUserReg();
+	val = (val & 0b01111110) | res;
+	uint8_t temp[2] = { SHT2x_WRITE_REG, val };
+	HAL_I2C_Master_Transmit(_sht2x_ui2c, SHT2x_I2C_ADDR << 1, temp, 2,
+			SHT2x_TIMEOUT);
+}
+
+/**
+ * @brief Converts degrees Celsius to degrees Fahrenheit.
+ * @param celsius Floating point temperature in degrees Celsius.
+ * @return Floating point temperature in degrees Fahrenheit.
+ */
+float SHT2x_CelsiusToFahrenheit(float celsius) {
+	return (9.0 / 5.0) * celsius + 32;
+}
+
+/**
+ * @brief Converts degrees Celsius to Kelvin.
+ * @param celsius Floating point temperature in degrees Celsius.
+ * @return Floating point temperature in Kelvin.
+ */
+float SHT2x_CelsiusToKelvin(float celsius) {
+	return celsius + 273;
+}
+
+/**
+ * @brief Gets the integer part of a floating point number.
+ * @note Avoids the use of sprinf %f floating point formatting.
+ * @param num Floating point number.
+ * @return Integer part of floating point number.
+ */
+int32_t SHT2x_Pre(float num) {
+	return num / 1;
+}
+
+/**
+ * @brief Gets the decimal part of a floating point number.
+ * @note Avoids the use of sprinf %f floating point formatting.
+ * @param num Floating point number.
+ * @return Decimal part of floating point number.
+ */
+uint32_t SHT2x_Post(float num, int digits) {
+	float postDec = num - SHT2x_Pre(num);
+	return postDec * SHT2x_Ipow(10, digits);
+}
+
+/**
+ * @brief Integer equivalent of pow() in math.h.
+ * @param base Base.
+ * @param power Power.
+ * @return
+ */
+uint32_t SHT2x_Ipow(uint32_t base, uint32_t power) {
+	uint32_t temp = base;
+	for (uint32_t i = 1; i < power; i++)
+		temp *= base;
+	return temp;
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ sht2x_for_stm32_hal.h
@@ -0,0 +1,42 @@
+#ifndef SHT2X_FOR_STM32_HAL_H
+#define SHT2X_FOR_STM32_HAL_H
+#include "main.h"
+/*----------------------------------------------------------------------------*/
+#define SHT2x_I2C_ADDR			0x40
+#define SHT2x_HOLD_MASTER		1
+#define SHT2x_READ_TEMP_HOLD	0b11100011
+#define	SHT2x_READ_RH_HOLD		0b11100101
+#define SHT2x_READ_TEMP_NOHOLD	0b11110011
+#define SHT2x_READ_RH_NOHOLD	0b11110101
+#define	SHT2x_WRITE_REG			0b11100110
+#define SHT2x_READ_REG			0b11100111
+#define SHT2x_SOFT_RESET		0b11111110
+#define SHT2x_TIMEOUT			1000
+/*----------------------------------------------------------------------------*/
+typedef enum Resolution {
+	RES_14_12 = 0b00000000,
+	RES_12_8 = 0b00000001,
+	RES_13_10 = 0b10000000,
+	RES_11_11 = 0b10000001,
+} Resolution;
+/*----------------------------------------------------------------------------*/
+I2C_HandleTypeDef *_sht2x_ui2c;
+
+void SHT2x_Init(I2C_HandleTypeDef *hi2c);
+void SHT2x_SetResolution(Resolution res);
+
+uint8_t SHT2x_ReadUserReg(void);
+
+uint16_t SHT2x_GetRaw(uint8_t cmd);
+
+float SHT2x_GetTemperature(uint8_t hold);
+float SHT2x_GetRelativeHumidity(uint8_t hold);
+
+float SHT2x_CelsiusToFahrenheit(float celsius);
+float SHT2x_CelsiusToKelvin(float celsius);
+
+int32_t SHT2x_Pre(float f);
+uint32_t SHT2x_Post(float f, int digits);
+uint32_t SHT2x_Ipow(uint32_t x, uint32_t y);
+
+#endif
