c5f856ba083150ae33e32c4161c0b17be3f54875
Anonymous User <62833867+eepj@users.noreply.github.com>
2020-05-08 16:00:03 +0800

    Update sht2x_for_stm32_hal.h

--- sht2x_for_stm32_hal.h
+++ sht2x_for_stm32_hal.h
@@ -6,20 +6,20 @@
 /*----------------------------------------------------------------------------*/
 #define SHT2x_I2C_ADDR			0x40
 #define SHT2x_HOLD_MASTER		1
-#define SHT2x_READ_TEMP_HOLD	0b11100011
-#define	SHT2x_READ_RH_HOLD		0b11100101
-#define SHT2x_READ_TEMP_NOHOLD	0b11110011
-#define SHT2x_READ_RH_NOHOLD	0b11110101
-#define	SHT2x_WRITE_REG			0b11100110
-#define SHT2x_READ_REG			0b11100111
-#define SHT2x_SOFT_RESET		0b11111110
+#define SHT2x_READ_TEMP_HOLD	0xe3
+#define	SHT2x_READ_RH_HOLD		0xe5
+#define SHT2x_READ_TEMP_NOHOLD	0xf3
+#define SHT2x_READ_RH_NOHOLD	0xf5
+#define	SHT2x_WRITE_REG			0xe6
+#define SHT2x_READ_REG			0xe7
+#define SHT2x_SOFT_RESET		0xfe
 #define SHT2x_TIMEOUT			1000
 /*----------------------------------------------------------------------------*/
 typedef enum SHT2x_Resolution {
-	RES_14_12 = 0b00000000,
-	RES_12_8 = 0b00000001,
-	RES_13_10 = 0b10000000,
-	RES_11_11 = 0b10000001,
+	RES_14_12 = 0x00,
+	RES_12_8 = 0x00,
+	RES_13_10 = 0x80,
+	RES_11_11 = 0x81,
 } SHT2x_Resolution;
 /*----------------------------------------------------------------------------*/
 I2C_HandleTypeDef *_sht2x_ui2c;
